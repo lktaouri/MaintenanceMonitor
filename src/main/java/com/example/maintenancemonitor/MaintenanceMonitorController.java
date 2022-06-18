1eded27f18f1656db6662a83ef0394281b5178b6
safwan zullash <wi21b030@technikum-wien.at>
2022-06-18 17:54:58 +0200

    added setMessage and sendMessage function

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -1,13 +1,23 @@
 package com.example.maintenancemonitor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MaintenanceMonitorController {
-
+    private String message;
     @RequestMapping("/")
     public String index(){
         return "Hallo! Willkommen zum MaintenanceMonitor!";
+    }
+    @RequestMapping("/set")
+    public String setMessage(@RequestParam String a){
+        message = a;
+        return String.format("Message set as: %s",message);
+    }
+    @RequestMapping("send")
+    public String sendMessage(){
+        return message;
     }
 }
